1ece65a50a01ce1ddec3a55ce3585a918903ef50
Edezio Ferreira <edezio@gmail.com>
2024-07-24 12:01:32 -0300

    Criação da classe PedidoService

---
+++ src/main/java/com/ecommerce/api/service/PedidoService.java
@@ -0,0 +1,44 @@
+package com.ecommerce.api.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.api.model.Pedido;
+import com.ecommerce.api.model.Produto;
+import com.ecommerce.api.repository.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public Pedido adicionarPedido(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }
+
+    public List<Pedido> listarPedidos() {
+        return pedidoRepository.findAll();
+    }
+
+    public Pedido obterPedido(Long id) {
+        Optional<Pedido> pedido = pedidoRepository.findById(id);
+        Double somaValor = 0.0;
+
+        if (pedido.isPresent()) {
+            for(Produto produto : pedido.get().getProdutos()) {
+                somaValor += produto.getPreco();
+            }
+            if (somaValor>200.0) {
+                pedido.get().setValorTotal(somaValor-(somaValor*0.1));
+            }
+            return pedido.get();
+        } else {
+            return null;
+        }
+    }
+
+}
