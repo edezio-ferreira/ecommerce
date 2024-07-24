aa062bbb1d8d86bf445f69e38a47bbd198176f68
Edezio Ferreira <edezio@gmail.com>
2024-07-24 12:34:14 -0300

    Criação do Endpoint para obter um produto específico

--- src/main/java/com/ecommerce/api/controller/ProdutoController.java
+++ src/main/java/com/ecommerce/api/controller/ProdutoController.java
@@ -1,11 +1,13 @@
 package com.ecommerce.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,11 @@ public class ProdutoController {
     @GetMapping
     public ResponseEntity<List<Produto>> listaProdutos() {
         return ResponseEntity.status(HttpStatus.OK).body(produtoService.listaProdutos());
-    } 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> obterProduto(@PathVariable Long id) {
+        Optional<Produto> produto = produtoService.obterProduto(id);
+        return produto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
